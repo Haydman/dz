8a8259cd68e68e47a2114a8604b0e584edc1fe6c
Haydman <shorokon28@gmail.com>
2022-10-16 00:15:15 +0300

    4.1

--- 1/2.cpp
+++ 1/2.cpp
@@ -4,12 +4,16 @@ using namespace std;
 
 int main()
 {
-	int a;
-	int b;
-	cin >> a >> b;
-	cout << a + b << "\n";
-	cout << a - b << "\n";
-	cout << a * b << "\n";
-	cout << a / b << "\n";
+	setlocale(LC_ALL, "Russian");
+	float a;
+	float b;
+	cout << "Введите a: ";
+	cin >> a;
+	cout << "Введите b: ";
+	cin >> b;
+	cout << "Сложение" << endl << a + b << "\n";
+	cout << "Вычитание" << endl << a - b << "\n";
+	cout << "Умножение" << endl << a * b << "\n";
+	cout << "Деление" << endl << a / b << "\n";
 
 }
